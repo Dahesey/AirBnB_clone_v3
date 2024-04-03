8a7e593c994b19c918ab1c397c7ffae7c60e390c
Cynthia Dodzi <dahesey@gmail.com>
2024-04-04 00:15:29 +0100

    Added unittests and models

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,58 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """ Tests the get method if it can fetch details"""
+        new = State(
+                id='coco',
+                name='Accra',
+                email='reinecoletteo@gmail.com',
+                password='17071990',
+                first_name='Reine',
+                last_name='Otoo'
+            )
+        dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER')
+                passwd=os.getenv('HBNB_MYSQL_PWD')
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+        if 'State.' + new.id not in storage.all().keys():
+            new.save()
+            self.assertTrue(new in storage.all().values())
+        dbc.close()
+        hold = storage.get(State, new.id)
+        self.assertEqual(new.id, hold.id)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """ Tests the count method"""
+        initial_count = len(storage.all())
+        initial_user_count = len(storage.all(User))
+        method_count = storage.count()
+        user_count = storage.count(User)
+        self.assertEqual(initial_count, method_count)
+        self.assertEqual(user_count, initial_user_count)
+        new = User(
+                email='dahesey@gmail.com',
+                password='17071990',
+                first_name='Cynthia',
+                last_name='Dodzi'
+             )
+        dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+        new.save()
+        dbc.close()
+        self.assertTrue(new in storage.all().values())
+        method_count = storage.count()
+        user_count = storage.count(User)
+        self.assertEqual(initial_count + 1, method_count)
+        self.assertEqual(initial_user_count + 1, user_count)
